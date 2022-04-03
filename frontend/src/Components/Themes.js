d1289d8b780af1445c93ac4c61e2dcaf40cee7f9
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2022-04-03 19:20:19 +0700

    CodeStyle in Themes

--- frontend/src/Components/Themes.js
+++ frontend/src/Components/Themes.js
@@ -6,22 +6,18 @@ const LightTheme = {
     tableColor: "#34495E",
     tableTitle: "#dd5",
     tableFontColor: "#fff",
-
 };
-
 const DarkTheme = {
     pageBackground: "#282c36",
     titleColor: "#ACC1D6",
     tableColor: "#ACC1D6",
     tableFontColor: "#000",
 }
+const  ThemeContext = React.createContext();
 
 export const Themes = {
     light: LightTheme,
     dark: DarkTheme,
     now: 'light',
 }
-
-//export const [theme, setTheme] = useState(Themes);
-const  ThemeContext = React.createContext();
 export default ThemeContext;
